40fe105f64faea3a1d7e025db2b167cbecdc893d
ArtGarm <artem.garmatyk@gmail.com>
2015-10-26 16:14:05 +0200

    xzdf

---
+++ partials/table.php
@@ -0,0 +1,22 @@
+<div class="long-table-wrap">
+    <div class="row">
+        <div class="col-xs-12"> Льготы для участников СЭЗ в Крыму</div>
+    </div>
+
+    <div class="table-responsive table-striped">
+        <table class="table ">
+        <thead>
+            <tr>
+                <th>Вид платежа</th>
+                <th>Льгота</th>
+                <th>Срок действия</th>
+                <th>Примечание</th>
+                <th>Норма</th>
+            </tr>
+
+        </thead>
+
+
+        </table>
+    </div>
+</div>
