e499c5f17bf2cdb666d7fcb5d2d2b858b0becb0c
Radiks <radion91@gmail.com>
2015-10-27 14:22:58 +0200

    responce 100%

--- partials/map.php
+++ partials/map.php
@@ -1,5 +1,5 @@
-<div class="map">
-    <div class="map_wrap">
+<div class="map animate-section">
+    <div class="map_wrap animate-it">
         <div id="google_map"></div>
     </div>
 </div>
---
+++ partials/responce.php
@@ -0,0 +1,22 @@
+<div class="responce_wrap animate-section">
+    <div class="responce animate-it">
+        <div class="container">
+            <div class="row">
+                <div class="responce_title text-center animate-it">
+                    <h2>
+                        Ответственность
+                    </h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="responce_descr text-center animate-it animate-it">
+                    <p>В случае установления нарушения исполнения договора Участником CЭЗ,</p>
+                    <p>договор расторгается и Участник обязан компенсировать сумму всех льгот, </p>
+                    <p>примененных за весь период их использования, с учетом штрафных санкций и пени.</p>
+                </div>
+            </div>
+        </div>
+        <div class="responce_triangle">
+        </div>
+    </div>
+</div>
