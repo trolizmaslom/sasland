ba9cb2951b7e8a31077889e93a988bb4d33d9a30
Dmitry <dmitry1940@mail.ru>
2015-10-27 14:42:50 +0200

    facts-about-us added markup

---
+++ partials/facts-about-us.php
@@ -0,0 +1,65 @@
+<div class="facts-about-us">
+    <div class="container">
+        <div class="row">
+            <div class="facts-about-us-bg">
+                <img src="images/facts-about-us-bg.png" alt="" />
+            </div>
+            <div class="facts-about-us-wrap cfix">
+                <div class="col-md-3 col-md-offset-1 inline-block">
+                    <div class="facts-about-title">
+                        <span class="facts-about-title-num" data-num="8"></span>
+                        <span class="facts-about-title-text">лет</span>
+                    </div>
+                    <div class="facts-about-text">
+                        На рынке юридических услуг в области налогового права и судебно-экономическо экспертизы.
+                    </div>
+                </div>
+                <div class="col-md-3 col-md-offset-1 inline-block">
+                    <div class="facts-about-title">
+                        <span class="facts-about-title-num" data-num="4"></span>
+                        <span class="facts-about-title-text">кандидата наук</span>
+                    </div>
+                    <div class="facts-about-text">
+                        3 экономических, 1 юридический Судебно-экономический эксперт Адвокат.
+                    </div>
+                </div>
+                <div class="col-md-3 inline-block">
+                    <div class="facts-about-title">
+                        <span class="facts-about-title-num" data-num="984"></span>
+                        <span class="facts-about-title-text">экспертизы</span>
+                    </div>
+                    <div class="facts-about-text">
+                        Судебно-экономические по налоговым спорам.
+                    </div>
+                </div>
+                <div class="col-md-3 col-md-offset-1 inline-block">
+                    <div class="facts-about-title">
+                        <span class="facts-about-title-num" data-num="16"></span>
+                        <span class="facts-about-title-text">стран</span>
+                    </div>
+                    <div class="facts-about-text">
+                        С которыми мы сотрудничали.
+                    </div>
+                </div>
+                <div class="col-md-3 col-md-offset-1 inline-block">
+                    <div class="facts-about-title">
+                        <span class="facts-about-title-num" data-num="28"></span>
+                        <span class="facts-about-title-text">участников СЭЗ</span>
+                    </div>
+                    <div class="facts-about-text">
+                        Из 129 мы зарегистрировали в Крыму которые сейчас экономят.
+                    </div>
+                </div>
+                <div class="col-md-3 inline-block">
+                    <div class="facts-about-title">
+                        <span class="facts-about-title-num" data-num="30"></span>
+                        <span class="facts-about-title-text">млн рублей</span>
+                    </div>
+                    <div class="facts-about-text">
+                        В месяц на налогах и сборах.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
