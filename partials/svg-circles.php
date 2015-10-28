c309e0000a0dd205dc041db1e92bb851c54bccf3
Radiks <radion91@gmail.com>
2015-10-28 10:19:10 +0200

    fix svg

--- partials/svg-circles.php
+++ partials/svg-circles.php
@@ -12,8 +12,8 @@
                     <div class="svg_item_wrap cfix">
                         <div class="svg_item item_1" data-value="18" data-text="до" data-percent="%">
                             <div class="svg_item_img">
-                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 300 300" preserveAspectRatio="none" style="width:300; height:300; top:0; left:0;">
-                                    <circle cx="100" cy="100" r="96" class="svg_circle" fill="none" stroke="#fff" stroke-width="2" stroke-dasharray="0,20000" transform="rotate(-90,100,100)" />
+                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" preserveAspectRatio="none" style="width:300; height:300; top:0; left:0;">
+                                    <circle cx="100" cy="100" r="96" class="svg_circle" fill="none" stroke="#fff" stroke-width="2" stroke-dasharray="0,20000" transform="rotate(-90,100,100)" height="192px" width="192px"/>
 
                                     <text class="svgText" text-anchor="middle" x="95" y="120" fill="#fff" style="font-size: 66px;font-family:PNova;font-weight: 100" ></text>
                                 </svg>
@@ -26,7 +26,7 @@
                         </div>
                         <div class="svg_item item_2" data-value="22.4" data-percent="%">
                             <div class="svg_item_img">
-                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 300 300" preserveAspectRatio="none" style="width:300; height:300; top:0; left:0;">
+                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" preserveAspectRatio="none" style="width:300; height:300; top:0; left:0;">
                                     <circle cx="100" cy="100" r="96" class="svg_circle" fill="none" stroke="#fff" stroke-width="2" stroke-dasharray="0,20000" transform="rotate(-90,100,100)" />
                                     <text class="svgText" text-anchor="middle" x="90" y="120" fill="#fff" style="font-size: 66px;font-family:PNova;font-weight: 100" ></text>
                                 </svg>
@@ -40,7 +40,7 @@
                         </div>
                         <div class="svg_item item_3" data-value="100" data-percent="%">
                             <div class="svg_item_img">
-                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 300 300" preserveAspectRatio="none" style="width:300; height:300; top:0; left:0;">
+                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" preserveAspectRatio="none" style="width:300; height:300; top:0; left:0;">
                                     <circle cx="100" cy="100" r="96" class="svg_circle" fill="none" stroke="#fff" stroke-width="2" stroke-dasharray="0,20000" transform="rotate(-90,100,100)" />
                                     <text class="svgText" text-anchor="middle" x="95" y="120" fill="#fff" style="font-size: 66px;font-family:PNova;font-weight: 100" ></text>
                                 </svg>
@@ -53,7 +53,7 @@
                         </div>
                         <div class="svg_item item_4" data-value="100" data-percent="%">
                             <div class="svg_item_img">
-                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 300 300" preserveAspectRatio="none" style="width:300; height:300; top:0; left:0;">
+                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" preserveAspectRatio="none" style="width:300; height:300; top:0; left:0;">
                                     <circle cx="100" cy="100" r="96" class="svg_circle" fill="none" stroke="#fff" stroke-width="2" stroke-dasharray="0,20000" transform="rotate(-90,100,100)" />
                                     <text class="svgText" text-anchor="middle" x="95" y="120" fill="#fff" style="font-size: 66px;font-family:PNova;font-weight: 100" ></text>
                                 </svg>
@@ -67,7 +67,7 @@
                         </div>
                         <div class="svg_item item_5" data-value="100" data-percent="%">
                             <div class="svg_item_img">
-                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 300 300" preserveAspectRatio="none" style="width:300; height:300; top:0; left:0;">
+                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" preserveAspectRatio="none" style="width:300; height:300; top:0; left:0;">
                                     <circle cx="100" cy="100" r="96" class="svg_circle" fill="none" stroke="#fff" stroke-width="2" stroke-dasharray="0,20000" transform="rotate(-90,100,100)" />
                                     <text class="svgText" text-anchor="middle" x="95" y="120" fill="#fff" style="font-size: 66px;font-family:PNova;font-weight: 100" ></text>
                                 </svg>
